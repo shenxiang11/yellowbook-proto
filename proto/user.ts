227c85ed326be9d66b3a3f8541f569f2b2cb5e7f
shenxiang <shenxiang@foursmile001.com>
2023-09-07 18:23:25 +0800

    feat: 用于展示的时间由后端处理

--- proto/user.ts
+++ proto/user.ts
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import * as _m0 from "protobufjs/minimal";
-import { Timestamp } from "../google/protobuf/timestamp";
 import Long = require("long");
 
 export const protobufPackage = "";
@@ -47,8 +46,8 @@ export interface User {
   nickname: string;
   birthday: string;
   introduction: string;
-  createTime: Date | undefined;
-  updateTime: Date | undefined;
+  createTime: string;
+  updateTime: string;
 }
 
 export interface GetUserListResponse {
@@ -559,16 +558,7 @@ export const LoginSMSRequest = {
 };
 
 function createBaseUser(): User {
-  return {
-    id: 0,
-    email: "",
-    phone: "",
-    nickname: "",
-    birthday: "",
-    introduction: "",
-    createTime: undefined,
-    updateTime: undefined,
-  };
+  return { id: 0, email: "", phone: "", nickname: "", birthday: "", introduction: "", createTime: "", updateTime: "" };
 }
 
 export const User = {
@@ -591,11 +581,11 @@ export const User = {
     if (message.introduction !== "") {
       writer.uint32(50).string(message.introduction);
     }
-    if (message.createTime !== undefined) {
-      Timestamp.encode(toTimestamp(message.createTime), writer.uint32(58).fork()).ldelim();
+    if (message.createTime !== "") {
+      writer.uint32(58).string(message.createTime);
     }
-    if (message.updateTime !== undefined) {
-      Timestamp.encode(toTimestamp(message.updateTime), writer.uint32(66).fork()).ldelim();
+    if (message.updateTime !== "") {
+      writer.uint32(66).string(message.updateTime);
     }
     return writer;
   },
@@ -654,14 +644,14 @@ export const User = {
             break;
           }
 
-          message.createTime = fromTimestamp(Timestamp.decode(reader, reader.uint32()));
+          message.createTime = reader.string();
           continue;
         case 8:
           if (tag !== 66) {
             break;
           }
 
-          message.updateTime = fromTimestamp(Timestamp.decode(reader, reader.uint32()));
+          message.updateTime = reader.string();
           continue;
       }
       if ((tag & 7) === 4 || tag === 0) {
@@ -680,8 +670,8 @@ export const User = {
       nickname: isSet(object.nickname) ? String(object.nickname) : "",
       birthday: isSet(object.birthday) ? String(object.birthday) : "",
       introduction: isSet(object.introduction) ? String(object.introduction) : "",
-      createTime: isSet(object.createTime) ? fromJsonTimestamp(object.createTime) : undefined,
-      updateTime: isSet(object.updateTime) ? fromJsonTimestamp(object.updateTime) : undefined,
+      createTime: isSet(object.createTime) ? String(object.createTime) : "",
+      updateTime: isSet(object.updateTime) ? String(object.updateTime) : "",
     };
   },
 
@@ -705,11 +695,11 @@ export const User = {
     if (message.introduction !== "") {
       obj.introduction = message.introduction;
     }
-    if (message.createTime !== undefined) {
-      obj.createTime = message.createTime.toISOString();
+    if (message.createTime !== "") {
+      obj.createTime = message.createTime;
     }
-    if (message.updateTime !== undefined) {
-      obj.updateTime = message.updateTime.toISOString();
+    if (message.updateTime !== "") {
+      obj.updateTime = message.updateTime;
     }
     return obj;
   },
@@ -725,8 +715,8 @@ export const User = {
     message.nickname = object.nickname ?? "";
     message.birthday = object.birthday ?? "";
     message.introduction = object.introduction ?? "";
-    message.createTime = object.createTime ?? undefined;
-    message.updateTime = object.updateTime ?? undefined;
+    message.createTime = object.createTime ?? "";
+    message.updateTime = object.updateTime ?? "";
     return message;
   },
 };
@@ -834,28 +824,6 @@ export type DeepPartial<T> = T extends Builtin ? T
 type KeysOfUnion<T> = T extends T ? keyof T : never;
 export type Exact<P, I extends P> = P extends Builtin ? P
   : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
-
-function toTimestamp(date: Date): Timestamp {
-  const seconds = date.getTime() / 1_000;
-  const nanos = (date.getTime() % 1_000) * 1_000_000;
-  return { seconds, nanos };
-}
-
-function fromTimestamp(t: Timestamp): Date {
-  let millis = (t.seconds || 0) * 1_000;
-  millis += (t.nanos || 0) / 1_000_000;
-  return new Date(millis);
-}
-
-function fromJsonTimestamp(o: any): Date {
-  if (o instanceof Date) {
-    return o;
-  } else if (typeof o === "string") {
-    return new Date(o);
-  } else {
-    return fromTimestamp(Timestamp.fromJSON(o));
-  }
-}
 
 function longToNumber(long: Long): number {
   if (long.gt(Number.MAX_SAFE_INTEGER)) {
